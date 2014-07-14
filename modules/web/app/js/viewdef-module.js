d9fb7d23551bc1cef491fa5c22348a3a27130c83
Suman Ganta <suman.g.ganta@oracle.com>
2014-07-13 23:30:07 -0700

    views, datasets initial working version

--- modules/web/app/js/viewdef-module.js
+++ modules/web/app/js/viewdef-module.js
@@ -1,15 +1,14 @@
-var formRenderer = angular.module('viewDefRender', ['dynform']);
+var formRenderer = angular.module('viewDefModule', ['dynform']);
 
-formRenderer.controller('viewDefRenderCtrl', ['$scope', '$http', 'viewDefService', function($scope, $http, viewDefService){
-    console.log('Promise is now resolved: ');
-    $scope.viewDefTemplate = JSON.parse(viewDefService.data.viewDefJson);;
+formRenderer.controller('viewDefCtrl', ['$scope', '$http', 'viewDefService', function($scope, $http, viewDefService){
+    $scope.viewDefTemplate = JSON.parse(viewDefService.data.viewDefJson);
+
     //this is model object, but dynamic-forms still mandates it to declare the object
     //(although regular model do not require it)
     $scope.formData = {};
     $scope.submitForm = function(){
     	alert('Form submitted' + angular.toJson($scope.formData));
     }
-    console.log("completed!");
 }]);
 
 formRenderer.filter('pretty', function() {
